b5f7e95028f827246a7ece9abd3cd194da3b7454
JanVHanssen <r0906558@ucll.be>
2026-05-12 15:37:11 +0200

    serviceAccount uit environment variable

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const serviceAccount = require('./serviceAccount.json');
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
